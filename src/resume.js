d56cc3ce6f42f9efbf94a301dbeb2f5000766910
Ossai Lucky <luckyblaqy@gmail.com>
2022-07-25 15:42:54 +0100

    first commit

---
+++ src/resume.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Resume = () => {
+  return (
+    <div>
+        <h1>My Coding Abilities</h1>
+        <p>Ossai Lucky is my name, and I work as a front end web developer.</p>
+
+        <p>I'm qualified for the position because I have knowledge of the listed languages (React.js, Vanilla JS, Bootstrap 4, HTML5, CSS3, Sass,) </p>
+        <p>I have experience working on both production-related and development-related projects.</p>
+    </div>
+  )
+}
+
+export default Resume
